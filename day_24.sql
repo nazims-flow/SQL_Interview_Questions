6552db8464be622aed0f72ad60aad709d111743e
Nazim Hussain Mazumder <84512979+nazims-flow@users.noreply.github.com>
2024-06-16 11:30:07 +0530

    Add files via upload

---
+++ day_24.sql
@@ -0,0 +1,58 @@
+-- 24/30 SQL Challenge
+
+--  ----------------------------------------------------------------------------------
+-- Questions 1
+--  ----------------------------------------------------------------------------------	
+
+-- SCHEMA 
+
+-- Dropping the table if it exists and then recreating it
+DROP TABLE IF EXISTS customers;
+
+-- Creating the customers table
+CREATE TABLE customers (
+    customer_id INT PRIMARY KEY,
+    first_name VARCHAR(50),
+    last_name VARCHAR(50),
+    email VARCHAR(100)
+);
+
+-- Inserting sample data into the customers table
+INSERT INTO customers (customer_id, first_name, last_name, email) VALUES
+(1, 'John', 'Doe', 'john.doe@example.com'),
+(2, 'Jane', 'Smith', 'jane.smith@example.com'),
+(3, 'Alice', 'Johnson', 'alice.johnson@example.com'),
+(4, 'Bob', 'Brown', 'bob.brown@example.com'),
+(5, 'Emily', 'Davis', 'john.doe@example.com'),
+(6, 'Michael', 'Williams', 'michael.w@example.com'),
+(7, 'David', 'Wilson', 'jane.smith@example.com'),
+(8, 'Sarah', 'Taylor', 'sarah.t@example.com'),
+(9, 'James', 'Anderson', 'james.a@example.com'),
+(10, 'Laura', 'Martinez', 'laura.m@example.com');
+
+
+/*
+
+
+Consider a table named customers with the following columns: 
+customer_id, first_name, last_name, and email. 
+Write an SQL query to find all the duplicate email addresses 
+in the customers table.
+
+*/
+-- approach 1
+SELECT email as duplicate_email
+FROM(
+SELECT email,
+       ROW_NUMBER() OVER(PARTITION BY email ORDER BY email) as rn
+	   FROM customers
+) WHERE rn >=2
+
+
+-- approach 2
+SELECT
+	email
+	-- COUNT(email) as cnt_frequency
+FROM customers
+GROUP BY email
+HAVING COUNT(email) > 1
